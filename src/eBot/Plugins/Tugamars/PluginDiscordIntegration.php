bc8b169ff20451b39df33c0acc283d44f2e20104
tugamars <mars.silva.13@gmail.com>
2022-02-15 01:58:16 +0000

    Added discord integration (webhooks)

--- src/eBot/Plugins/Tugamars/PluginDiscordIntegration.php
+++ src/eBot/Plugins/Tugamars/PluginDiscordIntegration.php
@@ -28,7 +28,7 @@ class PluginDiscordIntegration implements Plugin
 
     public function getEventList()
     {
-        return array(\eBot\Events\EventDispatcher::EVENT_ROUNDSCORED,\eBot\Events\EventDispatcher::EVENT_MATCH_END,\eBot\Events\EventDispatcher::EVENT_SAY,\eBot\Events\EventDispatcher::EVENT_KILL);
+        return array(\eBot\Events\EventDispatcher::EVENT_ROUNDSCORED,\eBot\Events\EventDispatcher::EVENT_MATCH_END,\eBot\Events\EventDispatcher::EVENT_SAY,\eBot\Events\EventDispatcher::EVENT_KILL,\eBot\Events\EventDispatcher::EVENT_MATCH_START,\eBot\Events\EventDispatcher::EVENT_HALFTIME_REACHED,\eBot\Events\EventDispatcher::EVENT_PAUSE_REQUESTED);
     }
 
     public function onStart()
@@ -79,9 +79,21 @@ class PluginDiscordIntegration implements Plugin
                 $sendTo=array('logs','live');
                 $embedMessage="Round ended";
                 break;
+            case \eBot\Events\EventDispatcher::EVENT_MATCH_START:
+                $sendTo=array('logs','live');
+                $embedMessage="Match started";
+                break;
             case \eBot\Events\EventDispatcher::EVENT_MATCH_END:
                 $sendTo=array('logs','live');
                 $embedMessage="Match ended";
+                break;
+            case \eBot\Events\EventDispatcher::EVENT_HALFTIME_REACHED:
+                $sendTo=array('logs','live');
+                $embedMessage="Halftime reached";
+                break;
+            case \eBot\Events\EventDispatcher::EVENT_PAUSE_REQUESTED:
+                $sendTo=array('logs','live');
+                $embedMessage=strtoupper($event->getType())." Pause requested by " . $event->getTeamName() . "(" . $event->getTeamSide() . ")";
                 break;
             case \eBot\Events\EventDispatcher::EVENT_KILL:
                 $sendTo=array('logs');
@@ -97,7 +109,7 @@ class PluginDiscordIntegration implements Plugin
             $embed=new DiscordWebhookEmbed();
 
             $matchInfo=$event->getMatch()->getMatchInformation();
-            $embed->addEmbed('','**'.$matchInfo["team_a"]["name"] . "** [" . $matchInfo["team_a"]["score"] . "] " . "vs [" . $matchInfo["team_b"]["score"] . "] **" . $matchInfo["team_b"]["name"] . "** #". $event->getMatch()->getMatchId() . "\n".$embedMessage,'',null,0x00aaf9);
+            $embed->addEmbed('','**'.$matchInfo["team_a"]["name"] . "** [" . $matchInfo["team_a"]["score"] . "] " . "vs [" . $matchInfo["team_b"]["score"] . "] **" . $matchInfo["team_b"]["name"] . "** #". $event->getMatch()->getMatchId() . " :information_source: " . $matchInfo["status_text"] ."\n".$embedMessage,'',null,0x00aaf9);
 
             DiscordTrait::sendMessageToWebhook($message,$this->webhooksUrl[$t],$embed);
         }
